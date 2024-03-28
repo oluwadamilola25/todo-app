fc11692ab5531c20254bb5b8f3b398cc71ada1f7
Damilola <oowoyokun@lighthaus-eko.com>
2024-03-28 12:49:57 +0100

    implemented the Ui and Ux of the todo app to the crude feature

---
+++ script.js
@@ -0,0 +1,276 @@
+// Selecting the id of the Title and Description Input fields
+const taskInput = document.getElementById("taskInput");
+const descriptionInput = document.getElementById("descriptionInput");
+
+// Selecting the id of the TaskList
+const taskList = document.getElementById("taskList");
+
+// Selecting the id of the Save Task  Button
+const taskButton = document.getElementById("taskButton");
+
+// Selecting the id of the Create Task  Button
+const createTaskButton = document.getElementById("createTask");
+
+// Selecting the Delete Modal
+const deleteModalAction = document.getElementById("confirmDelete");
+
+// Selecting the Modal Title
+const htmlUpdateTask = document.getElementById("modalLabel");
+
+// Selecting the  Content on the landing page
+const heroComponent = document.querySelector(".hero-component");
+
+// Disabled button function
+taskInput.addEventListener("input", function () {
+  if (taskInput.value.trim() === "") {
+    taskButton.disabled = true;
+  } else {
+    taskButton.disabled = false;
+  }
+});
+
+let currentTask = null;
+
+// Calling the Create new task function
+taskButton.addEventListener("click", addTask);
+
+// Task list array
+let tasks = [];
+
+function addTask(e) {
+  e.preventDefault();
+
+  // Get the task value
+  let taskValue = taskInput.value.trim();
+  let descriptionValue = descriptionInput.value.trim();
+
+  if (taskValue !== "") {
+    // Create task object
+    const taskObject = {
+      title: taskValue,
+      description: descriptionValue,
+    };
+
+    // Add the task to the array
+    tasks.push(taskObject);
+
+    // Update the task list on the User Interface
+    updateTaskList();
+
+    // landing page content action
+    landingPageContent();
+
+    // Show task list update
+    toast();
+
+    // Clear the input field
+    taskInput.value = "";
+    descriptionInput.value = "";
+
+    // Dismiss modal
+    closeModal(taskButton);
+
+    // Disable button
+    taskButton.disabled = true;
+  }
+}
+
+// landing page content action
+function landingPageContent() {
+  console.log(taskList);
+  if (taskList.innerHTML !== "") {
+    heroComponent.classList.add("no-show");
+  } else {
+    heroComponent.classList.remove("no-show");
+  }
+}
+
+// Error and Validation functin code
+taskButton.addEventListener("click", (e) => {
+  if (taskInput.value.trim() !== "") {
+    const form = document.querySelector(".needs-validation");
+    if (!form.checkValidity()) {
+      e.preventDefault(); // Prevent form submission if validation fails
+      e.stopPropagation(); // Stop event propagation to prevent other event handlers from executing
+    }
+    form.classList.add("was-validated"); // Add 'was-validated' class to display validation feedback
+  }
+});
+
+// Close Bootstrap Modal function
+function closeModal(button) {
+  button.setAttribute("data-bs-dismiss", "modal");
+  button.click();
+  button.removeAttribute("data-bs-dismiss");
+}
+
+// Task Update feedback message function
+function toast() {
+  const toast = document.querySelector("#myToast");
+  if (taskButton) {
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toast);
+
+    taskButton.addEventListener("click", () => {
+      toastBootstrap.show();
+    });
+  }
+}
+
+// Edit task function
+function editTask(index) {
+  console.log(index);
+
+  // Display Modal label title to Update Task
+  htmlUpdateTask.innerHTML = "Update Task";
+
+  // Show the former details of the task in the input fields.
+  taskInput.value = tasks[index].title;
+  descriptionInput.value = tasks[index].description;
+
+  // remove add task functionality from "save changes" button.
+  taskButton.removeEventListener("click", addTask);
+
+  // add update task functionality to the button.
+  taskButton.addEventListener("click", triggerTaskUpdate);
+
+  // Single call function that updates a task.
+  function triggerTaskUpdate() {
+    const newTitle = taskInput.value;
+    const newDescription = descriptionInput.value;
+
+    // Calling back the Update task  function
+    updateTask(index, newTitle, newDescription);
+
+    // recursion: the triggerTaskUpdate function removes itself from the save changes button.
+    taskButton.removeEventListener("click", triggerTaskUpdate);
+
+    // Add back the eventlistener to the save changes button
+    taskButton.addEventListener("click", addTask);
+
+    // Activate the close modal function on the save changes button
+    closeModal(taskButton);
+
+    // Disable button
+    taskButton.disabled = true;
+  }
+}
+
+function updateTask(index, newTitle, newDescription) {
+  //Object to re-assign the new task details into the selected task index
+  tasks[index] = {
+    title: newTitle,
+    description: newDescription,
+  };
+
+  // Update the task list on the User Interface
+  updateTaskList();
+
+  // Indictate successful update
+  toast();
+
+  // Clear the input field
+  taskInput.value = "";
+  descriptionInput.value = "";
+}
+
+// Update the task list on the User Interface function
+function updateTaskList() {
+  // Clear the current task list
+  console.log(tasks);
+  taskList.innerHTML = "";
+
+  // Add tasks from the array to the list dynamically
+  tasks.forEach((task, index) => {
+    const listItem = document.createElement("li");
+    listItem.className = " list-group-item  mt-5";
+    listItem.id = `list-${index}`;
+    listItem.innerHTML = `
+      <div class="d-flex justify-content-between">
+          <div class="d-flex gap-2">
+              <div>
+                  <input type="checkbox" class="form-check-input" id="remember" />
+                  <label for="remember" class="form-check-label"></label>
+              </div>
+              <div>
+                  <p class="line-through">${task.title}</p>
+                  <p class="line-through-two">${task.description}</p>
+              </div>
+          </div>
+          <div class="d-flex gap-1">
+              <div>
+                  <button class="btn btn-info btn-sm float-end me-1" data-bs-toggle="modal"
+                          data-bs-target="#exampleModal" onclick="editTask(${index})">Edit
+                  </button>
+              </div>
+              <div class="todo-item">
+                  <button type="button" class="btn btn-danger btn-sm float-end" data-bs-toggle="modal"
+                  data-bs-target="#deleteModal" onclick="deleteModal(${index})">Delete</button>
+              </div>
+          </div>
+      </div>
+  `;
+    taskList.appendChild(listItem);
+
+    // Event listener function to mark task as completed
+    const checkbox = listItem.querySelector(".form-check-input");
+    checkbox.addEventListener("change", function () {
+      const listGroupItem = listItem.querySelector(".line-through");
+      const listGroupItem2 = listItem.querySelector(".line-through-two");
+      if (this.checked) {
+        listGroupItem.classList.add("completed");
+        listGroupItem2.classList.add("completed");
+      } else {
+        listGroupItem.classList.remove("completed");
+        listGroupItem2.classList.remove("completed");
+      }
+    });
+  });
+}
+
+// Display delete modal function
+function deleteModal(index) {
+  currentTask = index;
+}
+
+// Function to confirm delete on the modal
+deleteModalAction.addEventListener("click", function () {
+  const currentTaskAsNumber = Number(currentTask);
+
+  // Perform deletion action here
+  tasks.splice(currentTaskAsNumber, 1);
+
+  // Update the task list on the User Interface
+  updateTaskList();
+
+  // Clear the input field
+  taskInput.value = "";
+  descriptionInput.value = "";
+
+  // landing page content action
+  landingPageContent();
+});
+
+// Selecting the cancel modal button
+const cancelModalButton = document.getElementById("cancelModalButton");
+
+// Selecting the cancel modal button
+const closeModalButton = document.getElementById("closeModalButton");
+
+// Clear input field function
+function clearInputFields() {
+  // Clear the input field
+  taskInput.value = "";
+  descriptionInput.value = "";
+
+  // Display Modal label title to Add Task
+  htmlUpdateTask.innerHTML = "Add Task";
+}
+
+// Event Listener function to clear fields with either of the buttons also
+cancelModalButton.addEventListener("click", clearInputFields);
+closeModalButton.addEventListener("click", clearInputFields);
+
+// Display Modal label title to Add Task function
+createTaskButton.addEventListener("click", function () {
+  htmlUpdateTask.innerHTML = "Add Task";
+});
